779344c686859459a7f7ed8d1a6c187b583821df
Siphiwe <giftbigguy@gmail.com>
2023-07-13 08:07:33 -0700

    territory does not make a nation

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Size is not grandeur
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+printf("size of a char: byte(s)\n", size of(char));
+printf("size of an int: byte(s)\n", size of(int));
+printf("size of a long int: byte(s)\n", size of(long int));
+printf("size of a long long int: byte(s)\n", size of(long long int));
+printf("size of a float: byte(s)\n", size of(float));
+return (0);
+}
