56ba08724120b541345e509314e7e0b6ea952363
Siphiwe <giftbigguy@gmail.com>
2023-07-13 06:28:46 -0700

    Hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,8 @@
  * main - Entry point
  *
  * Return: Always 0 (success)
- int main(void)
- {
- puts ("\"Programming is like building a multilingual puzzle");
- retun (0):
- }
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+			return (0);
+}
